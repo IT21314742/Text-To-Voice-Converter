1a96f06b456d26a3caf78ba4306d5521530cbb29
IT21314742 <it21314742@my.sliit.lk>
2024-06-01 23:07:37 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ let voiceSelect = document.querySelector("select");
 window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
+
     
 }
