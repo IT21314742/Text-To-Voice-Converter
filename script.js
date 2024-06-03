b6f8100fe2700ef31ddb09500aa34169700f452c
IT21314742 <it21314742@my.sliit.lk>
2024-06-03 22:01:46 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ voiceSelect.addEventListener("change", () => {
 
 
 document.querySelector("button").addEventListener("click", () => {
-    speech.te = document.querySelector("textarea").value;
+    speech.tex = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 })
