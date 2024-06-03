f85a4330c9385d17eb4ff26d1e8c1bc806962971
IT21314742 <it21314742@my.sliit.lk>
2024-06-03 21:57:50 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -20,5 +20,5 @@ voiceSelect.addEventListener("change", () => {
 
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(spee)
+    window.speechSynthesis.speak(speec)
 })
